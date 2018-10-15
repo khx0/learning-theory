0781f14444a4653a7756fdb5527ffc428d4524b3
Nikolas Schnellbächer <khx0@posteo.net>
2018-10-15 11:36:35 +0200

    added PMF norm check

--- src/entropy.py
+++ src/entropy.py
@@ -3,14 +3,28 @@
 ##########################################################################################
 # author: Nikolas Schnellbaecher
 # contact: nikolas.schnellbaecher@bioquant.uni-heidelberg.de
-# date: 2018-05-29
+# date: 2018-10-15
 # file: entropy.py
 ##########################################################################################
 
 import math
 import numpy as np
-    
+
+def checkPMFnorm(PMF, textString = '', verbose = False):
+    '''
+    Checks for proper normalization of a given probability mass function (PMF).
+    '''
+    norm = np.sum(PMF)
+    print "norm(" + textString + ") =", norm
+    if (not np.isclose(norm, 1.0)):
+        print textString + " distribution seems NOT to be normalized to 1."
+        if (verbose):
+            sys.exit(1)
+
 def checkPDFnorm(PDF, xGrid, textString = '', verbose = False):
+    '''
+    Checks for proper normalization of a given probability density function (PDF).
+    '''
     norm = np.trapz(PDF, xGrid)
     print("norm(" + textString + ") =", norm)
     if (not np.isclose(norm, 1.0)):
